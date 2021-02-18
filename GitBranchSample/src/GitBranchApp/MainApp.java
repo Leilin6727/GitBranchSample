3bb9054a08487703725f8e8c9573d80df3066256
YT Lin <leilin6727@gmail.com>
2021-02-18 14:57:14 +0800

    Add Math Branch

--- GitBranchSample/src/GitBranchApp/MainApp.java
+++ GitBranchSample/src/GitBranchApp/MainApp.java
@@ -5,6 +5,17 @@ public class MainApp {
 	public static void main(String[] args) {
 		//這是第一個版本
 		System.out.print("1st Version");
+		//這是一個新增數學運算的版本
+		Math cMath = new Math();
+		
+		System.out.println();
+		System.out.print(cMath.add(10, 5));
+		
 	}
 
+}
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
